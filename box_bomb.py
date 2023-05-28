307c90eb6d716a7a53ecc53473c7444b7ea0b265
AdriannaBieniek <102544492+AdriannaBieniek@users.noreply.github.com>
2023-05-28 21:54:41 +0200

    box_bomb class added

--- box_bomb.py
+++ box_bomb.py
@@ -1,13 +1,41 @@
 import pygame
+import random
+import tile
+
+def genre_boxes():
+    box=[]
+    bomb=[]
+    for index in range(len(tile.TileMap.box)):
+        box.append(Box(tile.TileMap.box[index][0],tile.TileMap.box[index][1],20,0))
+    for index in range(len(tile.TileMap.bomb)):
+        bomb.append(Box(tile.TileMap.bomb[index][0],tile.TileMap.bomb[index][1],20,1))
+        
+    return box,bomb
 
 class Box(object):
-    def __init__(self,health_points):
+    
+    def __init__(self,x,y,health_points,type):
         self.health_point=health_points
-
+        self.index=0
+        self.x=x
+        self.y=y
+        self.width=32
+        self.height=32
+        self.type=type #0-money 1- bomb
 
     def hit(self):
         self.health_point-=2
         if self.health_point==0:
-            print("Im dead")
-        print("hit")
+            if self.type==0:
+                Box.dead_money(self)
+            if self.type==1:
+                Box.dead_bomb(self)
+
+    def dead_money(self):
+        print("Im dead, a ty bedziesz bogaty")
+    def dead_bomb(self):
+        print("Im dead, i lap bombe")
+
+
+
 
--- bullet.py
+++ bullet.py
@@ -7,35 +7,39 @@ class Bullet(object):
         self.radius = radius
         self.color = color
         self.facing = facing
-        self.vel = 1
+        self.vel = 0.5
         self.rect = pygame.Rect(x, y, 4,4)
 
     def draw(self, win):
         pygame.draw.circle(win, self.color, (self.x,self.y), self.radius)
         
 
-    def shoot(screen,bullets,SCREEN_WIDTH,box):
+    def shoot(screen,bullets,SCREEN_WIDTH,boxes, bombs):
         
         for bullet in bullets:
+            if len(boxes)>0:
+                for boxindex in range(len(boxes)):    
+                    if bullet.x>(boxes[boxindex].x) and bullet.x<(boxes[boxindex].x+32):
+                        if bullet.y>(boxes[boxindex].y) and bullet.y<(boxes[boxindex].y+32):
+                            boxes[boxindex].hit()
+                            bullets.pop(bullets.index(bullet)) 
 
-            if bullet.x<(tile.TileMap.box[0][0]) and bullet.x>(tile.TileMap.box[0][0]+32):
-                print("Jestem w x")
-                if bullet.y<(tile.TileMap.box[0][0]) and bullet.y>(tile.TileMap.box[0][0]+32):
-                    print("jestem  w y")
-                    box.hit()
-                    bullets.pop(bullets.index(bullet)) 
+            if len(bombs)>0:
+                for bombindex in range(len(bombs)):    
+                    if bullet.x>(bombs[bombindex].x) and bullet.x<(bombs[bombindex].x+32):
+                        if bullet.y>(bombs[bombindex].y) and bullet.y<(bombs[bombindex].y+32):
+                            bombs[bombindex].hit()
+                            bullets.pop(bullets.index(bullet)) 
 
             if bullet.facing==0:
                 if bullet.x < SCREEN_WIDTH and bullet.x > 0:
                     bullet.x += bullet.vel  
-                    #bullet.draw(screen)
                 else:
                     bullets.pop(bullets.index(bullet)) 
                     
             if bullet.facing==1:
                 if bullet.x < SCREEN_WIDTH and bullet.x > 0:
                     bullet.x -= bullet.vel
-                    #bullet.draw(screen)
                 else:
                     bullets.pop(bullets.index(bullet)) 
 
