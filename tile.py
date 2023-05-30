b48390e7cd3c1d83f888da622ad7b20eda1730e8
Jacek Czyz <259265@student.pwr.edu.pl>
2023-05-31 01:03:55 +0200

    updates

--- tile.py
+++ tile.py
@@ -1,7 +1,7 @@
 import pygame
 from utility import csv_reader
-from box_bomb import Box
 from enemies import enemy_class
+
 TILE_SIZE = 32
 
 BLACK = (0, 0, 0)
@@ -12,6 +12,8 @@ BLUE = (0, 0, 255)
 YELLOW=(255,255,0)
 PURPLE=(38,43,226)
 MAGENTA=(255,0,255)
+
+
 
 
 
@@ -36,26 +38,22 @@ class TileMap():
     box = []
     bomb = []
     enemies = enemy_class()
+
     def __init__(self, data):
         self.width = len(data[0])
         self.height = len(data)
         self.tiles = []
         for y in range(self.height):
             row = []
-            for x in range(self.width):                
+            for x in range(self.width):
                 val = int(data[y][x])
                 collision = 1 if val in [-1, -2, 20, 30] else 0
-
- #               if collision==0:
- #                   for checked_enemy in range(len(self.enemies.enemy_list)):
- #                       if self.enemies.enemy_X[checked_enemy] > y*25 and self.enemies.enemy_X[checked_enemy] < (y+1)*25:
- #                           if self.enemies.enemy_Y[checked_enemy] > x*25 and self.enemies.enemy_Y[checked_enemy] < (x+1)*25:
-
-                for checked_enemy in range(len(self.enemies.enemy_list)):        
-                    if self.enemies.enemy_X[checked_enemy] > y*25 and self.enemies.enemy_X[checked_enemy] < (y+1)*25 and self.enemies.enemy_Y[checked_enemy] > x*25 and self.enemies.enemy_Y[checked_enemy] < (x+1)*25:
-                        self.enemies.if_draw[checked_enemy]=True
-
-
+                
+                if collision==0:
+                    for checked_enemy in range(len(self.enemies.enemy_list)):        
+                        if self.enemies.enemy_X[checked_enemy] > y*25 and self.enemies.enemy_X[checked_enemy] < (y+1)*25 and self.enemies.enemy_Y[checked_enemy] > x*25 and self.enemies.enemy_Y[checked_enemy] < (x+1)*25:
+                            self.enemies.if_draw[checked_enemy]=True
+                
                 if val == 1 or val == 2:
                     color = pygame.Color('white')
                 elif val == 0:
@@ -65,10 +63,8 @@ class TileMap():
                 elif val == 10:
                     color = pygame.Color('yellow')
                 elif val == 20:
-                    TileMap.box.append(tile.rect)
                     color = pygame.Color('purple')
                 elif val == 30:
-                    TileMap.bomb.append(tile.rect)
                     color = pygame.Color('magenta')
                 else:
                     color = pygame.Color('black')
@@ -77,14 +73,15 @@ class TileMap():
                     TileMap.cmap.append(tile.rect)  # Add tile to map of collisions
                 if val == 10:  # Check if tile is yellow
                     TileMap.dmap.append(tile.rect)  # Add tile to map of tiles that cause damage
+                if val ==20:
+                     TileMap.box.append(tile.rect)
+                if val==30:
+                     TileMap.bomb.append(tile.rect)
                 row.append(tile)
             self.tiles.append(row)
 
 
     def draw(self, screen):
-        print("Sdsdsdsdss")
         for row in self.tile_map:
             for tile in row:
                 tile.draw(screen)
-        self.enemies.display_enemy(screen)
-
