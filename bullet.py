840a06aa8dd62974c350a26ff9866355fc71c43c
JacekCzyz <72863363+JacekCzyz@users.noreply.github.com>
2023-06-12 00:08:41 +0200

    Wrzut ręczny, bo coś nie działało xd (przeciwnicy i strzelanie)

--- bullet.py
+++ bullet.py
@@ -1,6 +1,7 @@
 import pygame
-import tile
-import room
+from room import *
+from enemies import *
+from tile import *
 class Bullet(object):
     def __init__(self,x,y,radius,color,facing,power):
         self.x = x
@@ -16,10 +17,32 @@ class Bullet(object):
         pygame.draw.circle(win, self.color, (self.x,self.y), self.radius)
         
 
-    def shoot(bullets,SCREEN_WIDTH,SCREEN_HEIGHT, actualroom: room.Room):
+    def shoot(bullets,SCREEN_WIDTH,SCREEN_HEIGHT, cur_room, hero_bullet_power):
 
-        boxes = actualroom.boxesPower
-        bombs = actualroom.boxesHealth
+        boxes = cur_room.boxesPower
+        bombs = cur_room.boxesHealth
+
+
+        for bullet in bullets:
+            if bullet.facing==0:
+                dx = bullet.vel
+                dy=0
+            elif bullet.facing==1:
+                dx = -bullet.vel
+                dy = 0
+            elif bullet.facing==2:
+                dx=0
+                dy = bullet.vel
+            else:
+                dx=0
+                dy = -bullet.vel
+            if len(cur_room.enemies.enemy_list)>0:
+                future_rect = pygame.Rect(bullet.x+dx, bullet.y+dy, 4, 4)
+                for enemy in range(len(cur_room.enemies.enemy_list)):
+                    if future_rect.colliderect(cur_room.enemies.enemy_recs[enemy]):
+                        cur_room.enemies.hit(enemy, hero_bullet_power)
+                        bullets.pop(bullets.index(bullet))
+                        break
 
         for bullet in bullets:
             if len(boxes)>0:
@@ -39,26 +62,44 @@ class Bullet(object):
                             bullets.pop(bullets.index(bullet)) 
                             break
 
+
+
             if bullet.facing==0: #w prawo leci pocisk
                 if bullet.x < SCREEN_WIDTH and bullet.x > 0:
-                    bullet.x += bullet.vel  
+                    future_rect = pygame.Rect(bullet.x+bullet.vel, bullet.y, 4, 4)
+                    if not future_rect.collidelistall(tile.TileMap.collision_map):
+                        bullet.x += bullet.vel
+                    else:
+                        bullets.pop(bullets.index(bullet))
                 else:
                     bullets.pop(bullets.index(bullet)) 
                     
             if bullet.facing==1: #w lewo leci pocisk
                 if bullet.x < SCREEN_WIDTH and bullet.x > 0:
-                    bullet.x -= bullet.vel
+                    future_rect = pygame.Rect(bullet.x-bullet.vel, bullet.y, 4, 4)
+                    if not future_rect.collidelistall(tile.TileMap.collision_map):
+                        bullet.x -= bullet.vel
+                    else:
+                        bullets.pop(bullets.index(bullet))
                 else:
                     bullets.pop(bullets.index(bullet)) 
 
             if bullet.facing==2: #w dol
                 if bullet.y < SCREEN_HEIGHT and bullet.y > 0:
-                    bullet.y += bullet.vel
+                    future_rect = pygame.Rect(bullet.x, bullet.y+bullet.vel, 4, 4)
+                    if not future_rect.collidelistall(tile.TileMap.collision_map):
+                        bullet.y += bullet.vel
+                    else:
+                        bullets.pop(bullets.index(bullet))
                 else:
                     bullets.pop(bullets.index(bullet)) 
 
             if bullet.facing==3: #w gore
                 if bullet.y < SCREEN_HEIGHT and bullet.y > 0:
-                    bullet.y -= bullet.vel
+                    future_rect = pygame.Rect(bullet.x, bullet.y-bullet.vel, 4, 4)
+                    if not future_rect.collidelistall(tile.TileMap.collision_map):
+                        bullet.y -= bullet.vel
+                    else:
+                        bullets.pop(bullets.index(bullet))
                 else:
-                    bullets.pop(bullets.index(bullet))   
+                    bullets.pop(bullets.index(bullet))  
