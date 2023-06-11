b6c08fa112106d9afcdc167d03183ac282388968
JacekCzyz <72863363+JacekCzyz@users.noreply.github.com>
2023-06-12 00:09:36 +0200

    Wrzut ręczny, bo coś nie działało xd (przeciwnicy i strzelanie)

--- main.py
+++ main.py
@@ -13,11 +13,13 @@ clock = pygame.time.Clock()
 
 # Create an instance of the MapGenerator
 generator = MapGenerator(9, 13)
-generator.generate_map()
+generator.generate_map(screen)
 spawn = generator.get_spawn_room()
 actual_room = spawn
 character = Character(3, 3)
+enemy_check=True
 bullets=[]
+bullet_counter=0
 # Create the TileMap object
 TILE_SIZE = 40
 
@@ -48,26 +50,28 @@ while running:
         character.move_down()
     screen.fill((255, 255, 255))  # Fill the screen with white
     if key[pygame.K_c]:
-        if len(bullets) < 5: #max 5 pociski
-            facing=character.orientation # w ktora maja leciec 
-            bullets.append(Bullet(character.x , character.y , 4, (0,0,0), facing,character.causing_damage)) 
+        if len(bullets) < 3: #max 3 pociski
+            if bullet_counter%10==0:
+                bullets.append(Bullet(character.x , character.y , 4, (0,0,0), character.orientation, character.causing_damage))
 
-       
-    Bullet.shoot(bullets,WINDOW_SIZE[0],WINDOW_SIZE[1], actual_room)
     # Draw the tile map
     actual_room.draw(screen)
-
+       
+    Bullet.shoot(bullets, screen.get_size()[0], screen.get_size()[1], actual_room, character.causing_damage)
     for bullet in bullets:
         bullet.draw(screen)
-    #print(actual_room.get_coordinates())
+
+    actual_room.place_enemies(enemy_check)
+    actual_room.enemies.MoveAndDo((character.x, character.y))
 
     # Display the map
+    #generator.display_map(screen, actual_room.get_coordinates())
     generator.display_map(screen, actual_room.get_coordinates())
-    generator.display_map(screen, actual_room.get_coordinates())
-    actual_room = Character.update(character, actual_room, generator)
+    actual_room, enemy_check = Character.update(character, actual_room, generator)
     pygame.draw.rect(screen, character.color, character.rect)
 
     pygame.display.flip()
     clock.tick(60)
+    bullet_counter+=1
 
 pygame.quit()
